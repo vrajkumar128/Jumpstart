400f8911f10fb228a578ad595f7afc6236fd7625
vrrajkum <vivek.rajkumar@protonmail.com>
2018-01-29 21:14:36 -0500

    Add carousel functionality.

---
+++ resources/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $('.carousel').slick({
+    autoplay: true,
+    autoplaySpeed: 4000,
+    arrows: false,
+    draggable: false,
+    pauseOnFocus: false,
+    pauseOnHover: false
+  });
+});
